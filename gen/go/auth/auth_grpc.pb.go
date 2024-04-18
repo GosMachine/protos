72d27dd6787ad8e6af25e6e2e0a57b93b70622e6
gosmach1ne <1234567yhb@gmail.com>
2024-04-18 11:49:41 +0300

    email verify

--- gen/go/auth/auth_grpc.pb.go
+++ gen/go/auth/auth_grpc.pb.go
@@ -11,6 +11,7 @@ import (
 	grpc "google.golang.org/grpc"
 	codes "google.golang.org/grpc/codes"
 	status "google.golang.org/grpc/status"
+	emptypb "google.golang.org/protobuf/types/known/emptypb"
 )
 
 // This is a compile-time assertion to ensure that this generated file
@@ -26,6 +27,7 @@ type AuthClient interface {
 	Login(ctx context.Context, in *LoginRequest, opts ...grpc.CallOption) (*LoginResponse, error)
 	OAuth(ctx context.Context, in *OAuthRequest, opts ...grpc.CallOption) (*OAuthResponse, error)
 	EmailVerified(ctx context.Context, in *EmailVerifiedRequest, opts ...grpc.CallOption) (*EmailVerifiedResponse, error)
+	EmailVerify(ctx context.Context, in *EmailVerifyRequest, opts ...grpc.CallOption) (*emptypb.Empty, error)
 }
 
 type authClient struct {
@@ -72,6 +74,15 @@ func (c *authClient) EmailVerified(ctx context.Context, in *EmailVerifiedRequest
 	return out, nil
 }
 
+func (c *authClient) EmailVerify(ctx context.Context, in *EmailVerifyRequest, opts ...grpc.CallOption) (*emptypb.Empty, error) {
+	out := new(emptypb.Empty)
+	err := c.cc.Invoke(ctx, "/auth.Auth/EmailVerify", in, out, opts...)
+	if err != nil {
+		return nil, err
+	}
+	return out, nil
+}
+
 // AuthServer is the server API for Auth service.
 // All implementations must embed UnimplementedAuthServer
 // for forward compatibility
@@ -80,6 +91,7 @@ type AuthServer interface {
 	Login(context.Context, *LoginRequest) (*LoginResponse, error)
 	OAuth(context.Context, *OAuthRequest) (*OAuthResponse, error)
 	EmailVerified(context.Context, *EmailVerifiedRequest) (*EmailVerifiedResponse, error)
+	EmailVerify(context.Context, *EmailVerifyRequest) (*emptypb.Empty, error)
 	mustEmbedUnimplementedAuthServer()
 }
 
@@ -98,6 +110,9 @@ func (UnimplementedAuthServer) OAuth(context.Context, *OAuthRequest) (*OAuthResp
 }
 func (UnimplementedAuthServer) EmailVerified(context.Context, *EmailVerifiedRequest) (*EmailVerifiedResponse, error) {
 	return nil, status.Errorf(codes.Unimplemented, "method EmailVerified not implemented")
+}
+func (UnimplementedAuthServer) EmailVerify(context.Context, *EmailVerifyRequest) (*emptypb.Empty, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method EmailVerify not implemented")
 }
 func (UnimplementedAuthServer) mustEmbedUnimplementedAuthServer() {}
 
@@ -184,6 +199,24 @@ func _Auth_EmailVerified_Handler(srv interface{}, ctx context.Context, dec func(
 	return interceptor(ctx, in, info, handler)
 }
 
+func _Auth_EmailVerify_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+	in := new(EmailVerifyRequest)
+	if err := dec(in); err != nil {
+		return nil, err
+	}
+	if interceptor == nil {
+		return srv.(AuthServer).EmailVerify(ctx, in)
+	}
+	info := &grpc.UnaryServerInfo{
+		Server:     srv,
+		FullMethod: "/auth.Auth/EmailVerify",
+	}
+	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
+		return srv.(AuthServer).EmailVerify(ctx, req.(*EmailVerifyRequest))
+	}
+	return interceptor(ctx, in, info, handler)
+}
+
 // Auth_ServiceDesc is the grpc.ServiceDesc for Auth service.
 // It's only intended for direct use with grpc.RegisterService,
 // and not to be introspected or modified (even as a copy)
@@ -206,6 +239,10 @@ var Auth_ServiceDesc = grpc.ServiceDesc{
 		{
 			MethodName: "EmailVerified",
 			Handler:    _Auth_EmailVerified_Handler,
+		},
+		{
+			MethodName: "EmailVerify",
+			Handler:    _Auth_EmailVerify_Handler,
 		},
 	},
 	Streams:  []grpc.StreamDesc{},

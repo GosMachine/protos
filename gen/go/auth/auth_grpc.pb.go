870fd2c73c2c52fb585eebbe7e9743f5b946274c
gosmach1ne <1234567yhb@gmail.com>
2024-04-19 12:41:31 +0300

    email change

--- gen/go/auth/auth_grpc.pb.go
+++ gen/go/auth/auth_grpc.pb.go
@@ -29,6 +29,7 @@ type AuthClient interface {
 	EmailVerified(ctx context.Context, in *EmailVerifiedRequest, opts ...grpc.CallOption) (*EmailVerifiedResponse, error)
 	EmailVerify(ctx context.Context, in *EmailVerifyRequest, opts ...grpc.CallOption) (*emptypb.Empty, error)
 	ChangePass(ctx context.Context, in *ChangePassRequest, opts ...grpc.CallOption) (*ChangePassResponse, error)
+	ChangeEmail(ctx context.Context, in *ChangeEmailRequest, opts ...grpc.CallOption) (*ChangeEmailResponse, error)
 }
 
 type authClient struct {
@@ -93,6 +94,15 @@ func (c *authClient) ChangePass(ctx context.Context, in *ChangePassRequest, opts
 	return out, nil
 }
 
+func (c *authClient) ChangeEmail(ctx context.Context, in *ChangeEmailRequest, opts ...grpc.CallOption) (*ChangeEmailResponse, error) {
+	out := new(ChangeEmailResponse)
+	err := c.cc.Invoke(ctx, "/auth.Auth/ChangeEmail", in, out, opts...)
+	if err != nil {
+		return nil, err
+	}
+	return out, nil
+}
+
 // AuthServer is the server API for Auth service.
 // All implementations must embed UnimplementedAuthServer
 // for forward compatibility
@@ -103,6 +113,7 @@ type AuthServer interface {
 	EmailVerified(context.Context, *EmailVerifiedRequest) (*EmailVerifiedResponse, error)
 	EmailVerify(context.Context, *EmailVerifyRequest) (*emptypb.Empty, error)
 	ChangePass(context.Context, *ChangePassRequest) (*ChangePassResponse, error)
+	ChangeEmail(context.Context, *ChangeEmailRequest) (*ChangeEmailResponse, error)
 	mustEmbedUnimplementedAuthServer()
 }
 
@@ -127,6 +138,9 @@ func (UnimplementedAuthServer) EmailVerify(context.Context, *EmailVerifyRequest)
 }
 func (UnimplementedAuthServer) ChangePass(context.Context, *ChangePassRequest) (*ChangePassResponse, error) {
 	return nil, status.Errorf(codes.Unimplemented, "method ChangePass not implemented")
+}
+func (UnimplementedAuthServer) ChangeEmail(context.Context, *ChangeEmailRequest) (*ChangeEmailResponse, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method ChangeEmail not implemented")
 }
 func (UnimplementedAuthServer) mustEmbedUnimplementedAuthServer() {}
 
@@ -249,6 +263,24 @@ func _Auth_ChangePass_Handler(srv interface{}, ctx context.Context, dec func(int
 	return interceptor(ctx, in, info, handler)
 }
 
+func _Auth_ChangeEmail_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+	in := new(ChangeEmailRequest)
+	if err := dec(in); err != nil {
+		return nil, err
+	}
+	if interceptor == nil {
+		return srv.(AuthServer).ChangeEmail(ctx, in)
+	}
+	info := &grpc.UnaryServerInfo{
+		Server:     srv,
+		FullMethod: "/auth.Auth/ChangeEmail",
+	}
+	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
+		return srv.(AuthServer).ChangeEmail(ctx, req.(*ChangeEmailRequest))
+	}
+	return interceptor(ctx, in, info, handler)
+}
+
 // Auth_ServiceDesc is the grpc.ServiceDesc for Auth service.
 // It's only intended for direct use with grpc.RegisterService,
 // and not to be introspected or modified (even as a copy)
@@ -279,6 +311,10 @@ var Auth_ServiceDesc = grpc.ServiceDesc{
 		{
 			MethodName: "ChangePass",
 			Handler:    _Auth_ChangePass_Handler,
+		},
+		{
+			MethodName: "ChangeEmail",
+			Handler:    _Auth_ChangeEmail_Handler,
 		},
 	},
 	Streams:  []grpc.StreamDesc{},

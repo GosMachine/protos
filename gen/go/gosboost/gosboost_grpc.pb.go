f4cb36aa72941de844abf9bfd25ff4097e664d97
gosmach1ne <1234567yhb@gmail.com>
2024-09-15 16:09:54 +0300

    optimized

--- gen/go/gosboost/gosboost_grpc.pb.go
+++ gen/go/gosboost/gosboost_grpc.pb.go
@@ -121,10 +121,15 @@ var GosBoostDB_ServiceDesc = grpc.ServiceDesc{
 	Metadata: "gosboost/gosboost.proto",
 }
 
+const (
+	GosBoost_TwitchFollowChannel_FullMethodName = "/gosboost.GosBoost/TwitchFollowChannel"
+)
+
 // GosBoostClient is the client API for GosBoost service.
 //
 // For semantics around ctx use and closing/ending streaming RPCs, please refer to https://pkg.go.dev/google.golang.org/grpc/?tab=doc#ClientConn.NewStream.
 type GosBoostClient interface {
+	TwitchFollowChannel(ctx context.Context, in *TwitchFollowChannelRequest, opts ...grpc.CallOption) (*emptypb.Empty, error)
 }
 
 type gosBoostClient struct {
@@ -135,10 +140,21 @@ func NewGosBoostClient(cc grpc.ClientConnInterface) GosBoostClient {
 	return &gosBoostClient{cc}
 }
 
+func (c *gosBoostClient) TwitchFollowChannel(ctx context.Context, in *TwitchFollowChannelRequest, opts ...grpc.CallOption) (*emptypb.Empty, error) {
+	cOpts := append([]grpc.CallOption{grpc.StaticMethod()}, opts...)
+	out := new(emptypb.Empty)
+	err := c.cc.Invoke(ctx, GosBoost_TwitchFollowChannel_FullMethodName, in, out, cOpts...)
+	if err != nil {
+		return nil, err
+	}
+	return out, nil
+}
+
 // GosBoostServer is the server API for GosBoost service.
 // All implementations must embed UnimplementedGosBoostServer
 // for forward compatibility.
 type GosBoostServer interface {
+	TwitchFollowChannel(context.Context, *TwitchFollowChannelRequest) (*emptypb.Empty, error)
 	mustEmbedUnimplementedGosBoostServer()
 }
 
@@ -149,6 +165,9 @@ type GosBoostServer interface {
 // pointer dereference when methods are called.
 type UnimplementedGosBoostServer struct{}
 
+func (UnimplementedGosBoostServer) TwitchFollowChannel(context.Context, *TwitchFollowChannelRequest) (*emptypb.Empty, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method TwitchFollowChannel not implemented")
+}
 func (UnimplementedGosBoostServer) mustEmbedUnimplementedGosBoostServer() {}
 func (UnimplementedGosBoostServer) testEmbeddedByValue()                  {}
 
@@ -170,13 +189,36 @@ func RegisterGosBoostServer(s grpc.ServiceRegistrar, srv GosBoostServer) {
 	s.RegisterService(&GosBoost_ServiceDesc, srv)
 }
 
+func _GosBoost_TwitchFollowChannel_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+	in := new(TwitchFollowChannelRequest)
+	if err := dec(in); err != nil {
+		return nil, err
+	}
+	if interceptor == nil {
+		return srv.(GosBoostServer).TwitchFollowChannel(ctx, in)
+	}
+	info := &grpc.UnaryServerInfo{
+		Server:     srv,
+		FullMethod: GosBoost_TwitchFollowChannel_FullMethodName,
+	}
+	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
+		return srv.(GosBoostServer).TwitchFollowChannel(ctx, req.(*TwitchFollowChannelRequest))
+	}
+	return interceptor(ctx, in, info, handler)
+}
+
 // GosBoost_ServiceDesc is the grpc.ServiceDesc for GosBoost service.
 // It's only intended for direct use with grpc.RegisterService,
 // and not to be introspected or modified (even as a copy)
 var GosBoost_ServiceDesc = grpc.ServiceDesc{
 	ServiceName: "gosboost.GosBoost",
 	HandlerType: (*GosBoostServer)(nil),
-	Methods:     []grpc.MethodDesc{},
-	Streams:     []grpc.StreamDesc{},
-	Metadata:    "gosboost/gosboost.proto",
+	Methods: []grpc.MethodDesc{
+		{
+			MethodName: "TwitchFollowChannel",
+			Handler:    _GosBoost_TwitchFollowChannel_Handler,
+		},
+	},
+	Streams:  []grpc.StreamDesc{},
+	Metadata: "gosboost/gosboost.proto",
 }
